52753de811c2930c2cd4f2815e8fac345165bff1
Benedict <z5457257@ad.unsw.edu.au>
2024-09-11 18:52:46 +1000

    Checkpoint, midst of tut1

---
+++ tutorials/1/pointer.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	int a = 5;
+	int b = 123;
+
+	int *pa = &a;
+	int *pb = &b;
+
+	// printf("a = %d, b = %d\n", a, b);
+
+	*pa = 6;
+	*pb = 234;
+
+	// printf("a = %d, b = %d\n", a, b);
+
+	int c = *pa;
+	*pa = *pb;
+	*pb = c;
+
+	// printf("a = %d, b = %d\n", a, b);
+	
+	pa = pb;
+	*pa = 345;
+
+	// printf("a = %d, b = %d\n", a, b);
+	
+	return 0;
+}
---
+++ tutorials/1/tut1.md
@@ -0,0 +1,13 @@
+# Introductions (10 - 15 Minutes)
+- What is your name?
+- What COMP courses have you done?
+- Explain the latest photo in your camera roll?
+
+# Pointers
+Revision on Memory
+
+
+# Malloc
+
+
+# Linked Lists
